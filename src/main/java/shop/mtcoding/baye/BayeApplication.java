de217654394bec9174c5274de6aa5a1217ff2484
EunHeeKang <swumpo@naver.com>
2023-01-17 14:11:47 +0900

    프로젝트 초기화

---
+++ src/main/java/shop/mtcoding/baye/BayeApplication.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.baye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BayeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BayeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shop/mtcoding/baye/BayeApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.baye;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BayeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
